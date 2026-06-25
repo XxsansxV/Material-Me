0c632095ffd35d2d12246cf1e2e11c7325f51bf1
XxsansxV <93177301+XxsansxV@users.noreply.github.com>
2026-06-25 16:40:21 +0700

    added fish and fastfetch config yayyy

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+alias dotgit='git --git-dir=$HOME/.dotfiles-backup --work-tree=$HOME'
+alias pls='sudo'
+alias bigfetch="fastfetch -c all.jsonc"
+
+# overwrite greeting
+# potentially disabling fastfetch
+
+# I tried delaying fish_greeting here so that it would fit, turns out it wasn't because of the window size lmao
+# function fish_greeting
+#     sleep 0.05 && fastfetch
+# end
+
+# thefuck --alias | source

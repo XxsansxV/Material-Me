aaa9ab601c1b5d769ecdc16a021bbde72a784e2d
XxsansxV <93177301+XxsansxV@users.noreply.github.com>
2026-07-21 17:07:44 +0700

    finished moving GmB to local bin cus y

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ alias dotgit='git --git-dir=$HOME/.dotfiles-backup --work-tree=$HOME'
 alias pls='sudo'
 alias bigfetch="fastfetch -c all.jsonc"
 alias tinyfetch="fastfetch -c small.jsonc"
-alias GreetmeBash="bash $XDG_CONFIG_HOME/fish/GreetMeInBash.sh"
+# alias GreetmeBash="bash $XDG_CONFIG_HOME/fish/GreetMeInBash.sh"
 
 # overwrite greeting
 # potentially disabling fastfetch
